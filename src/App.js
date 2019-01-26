00a41d18f27cbd4f1795227c6d758ad520e4342e
Max <mjann@ucsc.edu>
2019-01-26 14:18:34 -0800

    idk where I'm going

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
+import './App.css';
+// JSX uses className instead of class for attributes
 class App extends Component {
+
+  // introduction to states
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the garbage',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Walk the dog',
+        completed: false
+      }
+    ]
+  }
+
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id == id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) });
+    console.log('From app.js bro');
+    console.log(id);
+    console.log(`from ${id} bro`);
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id != id)] });
+    console.log(`${id} was deleted`)
+  }
+
+// kinda like view from django
+  // Add Todo
+  addTodo = (title) => {
+    console.log(title);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          < Header />
+          < AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
