c5dc0954132b158e61733505abfe35da607df360
Max <mjann@ucsc.edu>
2019-01-26 14:52:18 -0800

    finished part 1 of tutorial pre http

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+// react router
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0. It is part of a React crash course yo</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
